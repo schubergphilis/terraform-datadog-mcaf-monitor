183b462733b9ecc5c915d785f7729ae614d217a7
Sander van Harmelen <sander@vanharmelen.nl>
2020-01-14 08:17:24 +0100

    Refactor the module a little

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
-  map_to_list = [for k, v in var.tag_map : "${k}:${v}"]
-  tags        = concat(var.tag_list, local.map_to_list)
+  tag_map = [for k, v in var.tag_map : "${k}:${v}"]
+  tags    = concat(var.tag_list, local.tag_map)
 }
 
 resource "datadog_monitor" "monitor" {
@@ -8,12 +8,12 @@ resource "datadog_monitor" "monitor" {
   type              = var.type
   message           = var.message
   query             = var.query
-  thresholds        = var.thresholds
-  notify_no_data    = var.notify_no_data
-  no_data_timeframe = var.no_data_timeframe
-  renotify_interval = var.renotify_interval
-  timeout_h         = var.timeout_h
   include_tags      = var.include_tags
   new_host_delay    = var.new_host_delay
+  no_data_timeframe = var.no_data_timeframe
+  notify_no_data    = var.notify_no_data
+  renotify_interval = var.renotify_interval
+  thresholds        = var.thresholds
+  timeout_h         = var.timeout
   tags              = local.tags
 }
