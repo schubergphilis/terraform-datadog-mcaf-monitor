651f6442a809fca84758bd7c5ca3a1604248b670
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2024-09-13 10:19:59 +0200

    breaking: Move monitor specific attributes to monitor level and add missing attributes
    Signed-off-by: Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>

--- variables.tf
+++ variables.tf
@@ -4,74 +4,38 @@ variable "dashboard" {
   description = "The Datadog dashboard URL shown in the alert message"
 }
 
-variable "evaluation_delay" {
-  type        = number
-  default     = null
-  description = "Seconds to delay evaluation to ensure the monitor has a full data period"
-}
-
-variable "include_tags" {
-  type        = bool
-  default     = true
-  description = "Whether to insert the triggering tags into the monitoring title"
-}
-
 variable "monitors" {
   type = map(object({
-    message    = string
-    name       = string
-    query      = string
-    thresholds = map(string)
-    threshold_windows = object({
-      recovery_window = string
-      trigger_window  = string
-    })
-    type = string
+    name                = string
+    message             = string
+    query               = string
+    type                = string
+    enable_logs_sample  = optional(bool, null)
+    escalation_message  = optional(string, null)
+    evaluation_delay    = optional(number, null)
+    force_delete        = optional(bool, null)
+    include_tags        = optional(bool, null)
+    locked              = optional(bool, null)
+    new_host_delay      = optional(number, null)
+    no_data_timeframe   = optional(number, null)
+    notify_audit        = optional(bool, null)
+    notify_no_data      = optional(bool, null)
+    priority            = optional(string, null)
+    renotify_interval   = optional(number, null)
+    require_full_window = optional(bool, null)
+    tags                = optional(list(string), null)
+    threshold_windows   = optional(map(string), null)
+    thresholds          = optional(map(string), null)
+    timeout_h           = optional(number, null)
   }))
   default     = null
   description = "The set of monitor specific attributes per monitor"
-}
-
-variable "new_group_delay" {
-  type        = number
-  default     = null
-  description = "Seconds after booting before starting the evaluation of monitor results"
-}
-
-variable "no_data_timeframe" {
-  type        = number
-  default     = null
-  description = "The number of minutes before a monitor will notify when data stops reporting"
 }
 
 variable "notifiers" {
   type        = list(string)
   default     = []
   description = "The notifiers to which the alerts get send"
-}
-
-variable "notify_no_data" {
-  type        = bool
-  default     = true
-  description = "Whether this monitor will notify when data stops reporting"
-}
-
-variable "priority" {
-  type        = number
-  default     = null
-  description = "Integer from 1 (high) to 5 (low) indicating alert severity"
-}
-
-variable "renotify_interval" {
-  type        = number
-  default     = null
-  description = "The number of minutes before a monitor will re-notify on the current status"
-}
-
-variable "require_full_window" {
-  type        = bool
-  default     = true
-  description = "A boolean indicating whether the monitor needs a full window of data before it's evaluated"
 }
 
 variable "tag_list" {
@@ -84,10 +48,4 @@ variable "tag_map" {
   type        = map(string)
   default     = {}
   description = "A map of tags to assign to the monitor"
-}
-
-variable "timeout" {
-  type        = number
-  default     = null
-  description = "Hours of not reporting data before automatically resolving from a triggered state"
 }
